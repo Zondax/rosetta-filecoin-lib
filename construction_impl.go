89153e9d95f353c0bcb520eaa8e4a0aedcd035ab
Juan Leni <lenijuan@gmail.com>
2020-07-13 22:31:33 +0200

    initial support for address/sign/verify

--- construction_impl.go
+++ construction_impl.go
@@ -14,3 +14,71 @@
 *  limitations under the License.
 ********************************************************************************/
 package rosetta_filecoin_lib
+
+import "github.com/filecoin-project/go-address"
+import "github.com/filecoin-project/lotus/lib/sigs"
+import "github.com/filecoin-project/specs-actors/actors/crypto"
+
+type RosettaConstructionFilecoin struct {
+	Mainnet bool
+}
+
+func (r RosettaConstructionFilecoin) DeriveFromPublicKey(publicKey []byte) (string, error) {
+	addr, err := address.NewSecp256k1Address(publicKey)
+	if err != nil {
+		return "", err
+	}
+	// FIXME: go=address does not allow setting a network
+
+	return addr.String(), nil
+}
+
+func (r RosettaConstructionFilecoin) Sign(message []byte, privateKey []byte) ([]byte, error) {
+	signature, err := sigs.Sign(crypto.SigTypeSecp256k1, privateKey, message)
+	if err != nil {
+		return nil, err
+	}
+
+	return signature.MarshalBinary()
+}
+
+func (r RosettaConstructionFilecoin) Verify(message []byte, publicKey []byte, signature []byte) error {
+	sig := crypto.Signature{}
+	err := sig.UnmarshalBinary(signature)
+	if err != nil {
+		return err
+	}
+
+	addr, err := address.NewSecp256k1Address(publicKey)
+	if err != nil {
+		return err
+	}
+
+	return sigs.Verify(&sig, addr, message)
+}
+
+func (r RosettaConstructionFilecoin) ConstructPayment(request *PaymentRequest) (string, error) {
+	panic("implement me")
+}
+
+func (r RosettaConstructionFilecoin) ConstructMultisigPayment(request *MultisigPaymentRequest) (string, error) {
+	panic("implement me")
+}
+
+func (r RosettaConstructionFilecoin) ConstructSwapAuthorizedParty(request *MultisigPaymentRequest) (string, error) {
+	panic("implement me")
+}
+
+func (r RosettaConstructionFilecoin) SignTx(unsignedTransaction string, privateKey []byte) (string, error) {
+//	m := types.Message
+
+	panic("implement me")
+}
+
+func (r RosettaConstructionFilecoin) ParseTx(request *ParseTxRequest) (interface{}, error) {
+	panic("implement me")
+}
+
+func (r RosettaConstructionFilecoin) Hash(signedTx string) (string, error) {
+	panic("implement me")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/zondax/rosetta-filecoin-lib
 
 go 1.14
+
+require (
+	github.com/filecoin-project/go-address v0.0.2-0.20200504173055-8b6f2fb2b3ef
+	github.com/filecoin-project/lotus v0.4.1
+	github.com/filecoin-project/specs-actors v0.6.2-0.20200702170846-2cd72643a5cf
+)
