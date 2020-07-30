3a60cebb45ed0117cce9ab38174da53275f8a4c0
Juan Leni <juan.leni@zondax.ch>
2020-07-31 00:23:55 +0200

    add basic linter + fixes

--- construction_impl_test.go
+++ construction_impl_test.go
@@ -13,7 +13,7 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 ********************************************************************************/
-package rosetta_filecoin_lib
+package rosettaFilecoinLib
 
 import (
 	"encoding/base64"
