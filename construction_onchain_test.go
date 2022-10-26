5daff52df27a8ee18825cc35cebbe84ca26aaf14
Nicolas Camilo <58498506+negarciacamilo@users.noreply.github.com>
2022-10-26 15:34:30 -0300

    Added auth

--- construction_onchain_test.go
+++ construction_onchain_test.go
@@ -43,22 +43,27 @@ const Signer1short = "t01002"
 const Signer2long = "t1itpqzzcx6yf52oc35dgsoxfqkoxpy6kdmygbaja"
 const Signer2short = "t01003"
 
-func getCredentials() (string, string, error) {
+func getCredentials() (string, string, string, error) {
 	lotusURL, found := os.LookupEnv("LOTUS_URL")
 	if !found {
-		return "", "", fmt.Errorf("Lotus URL has not been defined")
+		return "", "", "", fmt.Errorf("Lotus URL has not been defined")
 	}
 
 	lotusJWT, found := os.LookupEnv("LOTUS_JWT")
 	if !found {
-		return "", "", fmt.Errorf("Lotus JWT has not been defined")
+		return "", "", "", fmt.Errorf("Lotus JWT has not been defined")
 	}
 
-	return lotusURL, lotusJWT, nil
+	auth, found := os.LookupEnv("AUTH_JWT")
+	if !found {
+		return "", "", "", fmt.Errorf("Authentication header not found")
+	}
+
+	return lotusURL, lotusJWT, auth, nil
 }
 
 func sendLotusRequest(method string, id int, params string) (map[string]interface{}, error) {
-	lotusURL, lotusJWT, err := getCredentials()
+	lotusURL, lotusJWT, auth, err := getCredentials()
 	if err != nil {
 		return nil, err
 	}
@@ -73,7 +78,8 @@ func sendLotusRequest(method string, id int, params string) (map[string]interfac
 
 	// Set headers
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+lotusJWT)
+	req.Header.Set("X-Lotus-JWT", fmt.Sprintf("Beaderer %s", lotusJWT))
+	req.Header.Set("Authorization", fmt.Sprintf("Beaderer %s", auth))
 
 	// Set client timeout
 	client := &http.Client{Timeout: time.Second * 60}
