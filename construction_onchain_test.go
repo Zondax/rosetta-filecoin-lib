16694fdbb847e08253247edc9ae60edd69c8cb86
rllola <rllola80@gmail.com>
2023-02-20 22:41:21 +0100

    change for testnet address

--- construction_onchain_test.go
+++ construction_onchain_test.go
@@ -20,17 +20,18 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"github.com/filecoin-project/go-state-types/big"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/stretchr/testify/assert"
 )
 
-const SourceAddress1 = "f1d2xrzcslx7xlbbylc5c3d5lvandqw4iwl6epxba"
-const SourceAddressSK = "f15716d3b003b304b8055d9cc62e6b9c869d56cc930c3858d4d7c31f5f53f14a"
-const DestAddress1 = "f17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy"
+const SourceAddress1 = "t1d2xrzcslx7xlbbylc5c3d5lvandqw4iwl6epxba"
+const SourceAddressSK = "t15716d3b003b304b8055d9cc62e6b9c869d56cc930c3858d4d7c31f5f53f14a"
+const DestAddress1 = "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy"
 
 const GasFeeCap = "10049794"
 const GasPremium = "10049470"
