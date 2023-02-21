5e47a98897337ad97b7b47abf6a2450cf3e7307f
Juan Leni <juan.leni@zondax.ch>
2023-02-21 19:51:52 +0100

    correct private key

--- construction_onchain_test.go
+++ construction_onchain_test.go
@@ -30,7 +30,7 @@ import (
 )
 
 const SourceAddress1 = "t1d2xrzcslx7xlbbylc5c3d5lvandqw4iwl6epxba"
-const SourceAddressSK = "t15716d3b003b304b8055d9cc62e6b9c869d56cc930c3858d4d7c31f5f53f14a"
+const SourceAddressSK = "f15716d3b003b304b8055d9cc62e6b9c869d56cc930c3858d4d7c31f5f53f14a"
 const DestAddress1 = "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy"
 
 const GasFeeCap = "10049794"
