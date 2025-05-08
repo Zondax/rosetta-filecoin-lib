5ef382b3c9cdcfea2004c53d3d95184472218812
Eric Mokaya <emagembe@gmail.com>
2025-05-08 12:05:40 +0300

    feat: load all actor versions

--- actors/actors.go
+++ actors/actors.go
@@ -1,10 +1,16 @@
 package actors
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+
 	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
+	"go.uber.org/zap"
 
 	// The following import is necessary to ensure that the init() function
 	// from the lotus build package is invoked.
@@ -15,20 +21,50 @@ import (
 	_ "github.com/filecoin-project/lotus/build"
 )
 
+type ActorCidMap map[string]cid.Cid
 type BuiltinActorsMetadata struct {
-	Network          string
-	Version          network.Version
-	ActorsNameCidMap map[string]cid.Cid
+	Network                   string
+	Version                   network.Version
+	ActorsNameCidMapByVersion map[network.Version]ActorCidMap
 }
 
 type BuiltinActors struct {
 	Metadata BuiltinActorsMetadata
 }
 
+func NewBuiltinActors(networkName string, loadAllActorVersions bool, lotusApi api.FullNode) (*BuiltinActors, error) {
+	networkVersion, err := lotusApi.StateNetworkVersion(context.Background(), types.EmptyTSK)
+	if err != nil {
+		zap.S().Errorf("could not get lotus network version!: %s", err.Error())
+		return nil, err
+	}
+
+	actorCids, err := loadActorCids(networkVersion, loadAllActorVersions, lotusApi)
+	if err != nil {
+		zap.S().Errorf("could not get actors cids!: %s", err.Error())
+		return nil, err
+	}
+
+	d, _ := json.MarshalIndent(actorCids, "", "  ")
+	fmt.Println(string(d))
+
+	metadata := BuiltinActorsMetadata{
+		Network:                   string(networkName),
+		Version:                   networkVersion,
+		ActorsNameCidMapByVersion: actorCids,
+	}
+
+	return &BuiltinActors{Metadata: metadata}, nil
+}
+
 func (a *BuiltinActors) IsActor(actorCode cid.Cid, actorName string) bool {
 	// Try the latest actors' version first
-	if a.Metadata.ActorsNameCidMap[actorName] == actorCode {
-		return true
+	for _, actors := range a.Metadata.ActorsNameCidMapByVersion {
+		if cid, ok := actors[actorName]; ok {
+			if actorCode == cid {
+				return true
+			}
+		}
 	}
 
 	// Try legacy actors
@@ -41,7 +77,7 @@ func (a *BuiltinActors) IsActor(actorCode cid.Cid, actorName string) bool {
 
 func (a *BuiltinActors) GetActorNameFromCid(actorCode cid.Cid) (string, error) {
 	// Try the latest actors' version first
-	for name, code := range a.Metadata.ActorsNameCidMap {
+	for name, code := range a.Metadata.ActorsNameCidMapByVersion[a.Metadata.Version] {
 		if actorCode == code {
 			return name, nil
 		}
@@ -57,9 +93,91 @@ func (a *BuiltinActors) GetActorNameFromCid(actorCode cid.Cid) (string, error) {
 }
 
 func (a *BuiltinActors) GetActorCid(name string) (cid.Cid, error) {
-	if cid, ok := a.Metadata.ActorsNameCidMap[name]; ok {
+	if cid, ok := a.Metadata.ActorsNameCidMapByVersion[a.Metadata.Version][name]; ok {
 		return cid, nil
 	}
 
 	return cid.Cid{}, fmt.Errorf("actor '%s' not found in metadata", name)
+}
+
+func (a *BuiltinActors) GetActorNameFromCidByVersion(actorCode cid.Cid, version network.Version) (string, error) {
+	// Try the latest actors' version first
+	for name, code := range a.Metadata.ActorsNameCidMapByVersion[version] {
+		if actorCode == code {
+			return name, nil
+		}
+	}
+
+	// Try legacy actors
+	name := builtin.ActorNameByCode(actorCode)
+	if name != UnknownStr {
+		return name, nil
+	}
+
+	return UnknownStr, fmt.Errorf("invalid actor code CID: %s", actorCode)
+}
+
+func (a *BuiltinActors) GetActorCidByVersion(name string, version network.Version) (cid.Cid, error) {
+	if cid, ok := a.Metadata.ActorsNameCidMapByVersion[version][name]; ok {
+		return cid, nil
+	}
+
+	return cid.Cid{}, fmt.Errorf("actor '%s' not found in metadata", name)
+}
+
+func loadActorCids(currentNetworkVersion network.Version, loadAllActorVersions bool, lotusApi api.FullNode) (map[network.Version]ActorCidMap, error) {
+	var (
+		numWorkers      = 5
+		networkVersions []network.Version
+		actorCidsMap    = make(map[network.Version]ActorCidMap)
+	)
+
+	if loadAllActorVersions {
+		zap.S().Info("loading all actor versions")
+		networkVersions = make([]network.Version, network.Version26+1)
+		for i := network.Version0; i <= network.Version26; i++ {
+			networkVersions[i] = i
+		}
+	} else {
+		zap.S().Info("loading current actor version")
+		networkVersions = []network.Version{currentNetworkVersion}
+	}
+
+	versionChannel := make(chan network.Version, len(networkVersions))
+	actorCidsChannel := make(chan map[network.Version]ActorCidMap)
+
+	for i := 0; i < numWorkers; i++ {
+		go func(i int) {
+			for version := range versionChannel {
+				fmt.Printf("worker %d: loading actor cids for version %d\n", i, version)
+				// todo: retry on network failure
+				actorCids, err := lotusApi.StateActorCodeCIDs(context.Background(), version)
+				if err != nil {
+					zap.S().Errorf("worker %d: error loading actor cids for version %d: %s", i, version, err.Error())
+				}
+				actorCidsChannel <- map[network.Version]ActorCidMap{version: actorCids}
+			}
+		}(i)
+	}
+
+	for _, networkVersion := range networkVersions {
+		fmt.Printf("main: sending version %d to worker\n", networkVersion)
+		versionChannel <- networkVersion
+	}
+
+	var received int
+	for actorCids := range actorCidsChannel {
+		fmt.Printf("received actor cids for version %d\n", received+1)
+		for version, actors := range actorCids {
+			actorCidsMap[version] = actors
+		}
+		received++
+		if received == len(networkVersions) {
+			break
+		}
+	}
+	close(versionChannel)
+	close(actorCidsChannel)
+
+	return actorCidsMap, nil
 }
--- construction_impl.go
+++ construction_impl.go
@@ -52,6 +52,7 @@ type RosettaConstructionFilecoin struct {
 type LotusRpcV1 api.FullNode
 
 const EthAddressLength = 20
+const LoadAllActorVersions = true
 
 // NewFilecoinRPCClient creates a new lotus rpc client
 func NewFilecoinRPCClient(url string, token string) (LotusRpcV1, error) {
@@ -78,35 +79,21 @@ func NewRosettaConstructionFilecoin(lotusApi api.FullNode) *RosettaConstructionF
 		}
 	}
 
-	networkVersion, err := lotusApi.StateNetworkVersion(context.Background(), types.EmptyTSK)
-	if err != nil {
-		zap.S().Errorf("could not get lotus network version!: %s", err.Error())
-		return nil
-	}
-
 	networkName, err := lotusApi.StateNetworkName(context.Background())
 	if err != nil {
 		zap.S().Errorf("could not get lotus network name!: %s", err.Error())
 		return nil
 	}
 
-	actorCids, err := lotusApi.StateActorCodeCIDs(context.Background(), networkVersion)
+	builtinActors, err := actors.NewBuiltinActors(string(networkName), LoadAllActorVersions, lotusApi)
 	if err != nil {
-		zap.S().Errorf("could not get actors cids!: %s", err.Error())
+		zap.S().Errorf("could not create builtin actors!: %s", err.Error())
 		return nil
-	}
-
-	zap.S().Infof("Got actors CIDs for network: '%s' version: '%d'", networkName, networkVersion)
-
-	metadata := actors.BuiltinActorsMetadata{
-		Network:          string(networkName),
-		Version:          networkVersion,
-		ActorsNameCidMap: actorCids,
 	}
 
 	return &RosettaConstructionFilecoin{
 		networkName:   string(networkName),
-		BuiltinActors: actors.BuiltinActors{Metadata: metadata},
+		BuiltinActors: *builtinActors,
 		online:        true,
 	}
 }
